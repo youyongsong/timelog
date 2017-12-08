34fdbf1a51d83d3c77f1c506cec6c8640d780d1e
youyongsong <youyongsong@gmail.com>
2017-12-08 10:51:41 +0800

    fix: use gunicorn server static files

--- timelog/site_config/urls.py
+++ timelog/site_config/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('^$', admin.site.urls),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
